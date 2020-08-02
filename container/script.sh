a8edad1a770b467a40f1638b700799327a461f22
Gihan Rangana <gihanrangana.mr@gmail.com>
2020-08-02 17:04:50 +0530

    Update script.sh

--- container/script.sh
+++ container/script.sh
@@ -14,7 +14,6 @@
             mysql -h$KMYSQL_HOST -uroot -p$KMYSQL_ROOT_PASSWORD -e "grant usage on *.* to $KMYSQL_USER@'%' identified by '$KMYSQL_DATABASE'"
             mysql -h$KMYSQL_HOST -uroot -p$KMYSQL_ROOT_PASSWORD -e "grant all privileges on $KMYSQL_DATABASE.* to $KMYSQL_USER@'%'"
             mysql -h$KMYSQL_HOST -uroot -p$KMYSQL_ROOT_PASSWORD -e "flush privileges;"
-            mysql -h$KMYSQL_HOST -u$KMYSQL_USER -pdevelop $KMYSQL_DATABASE < /var/www/MISP/INSTALL/MYSQL.sql
             mysql -h$KMYSQL_HOST -u$KMYSQL_USER -p$KMYSQL_PASSWORD $KMYSQL_DATABASE < /var/www/MISP/INSTALL/MYSQL.sql
             touch /var/lib/mysql/.db_initialized
             chown -R mysql:mysql /var/lib/mysql
